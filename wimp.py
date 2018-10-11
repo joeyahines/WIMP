34c095f4dab3aa4550c0d93c43a0005bf534748d
Joey Hines <joey@ahines.net>
2018-10-11 14:45:16 -0500

    New code to detect shapes instead of color groups.

--- wimp.py
+++ wimp.py
@@ -1,43 +1,51 @@
 # import the necessary packages
-import numpy as np
 import cv2
-
-errplus = lambda t: t + (t * 0.05)
-errorplusvev = np.vectorize(errplus)
-errminus = lambda t: t - (t * 0.05)
-errorminusvev = np.vectorize(errminus)
-
-# load the image
-org_image = cv2.imread("test2.png")
-
-# define the list of boundaries
-colors = [
-    [65, 200, 230],
-    [95, 200, 240],
-    [230, 229, 236],
-    [70, 190, 240],
-]
-low = np.array([225, 225, 225], dtype="uint8")
-up = np.array([255, 255, 255], dtype="uint8")
-mask = cv2.inRange(org_image, low, up)
-image = cv2.bitwise_and(org_image, org_image, mask=255 - mask)
+import imutils
+import numpy as np
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-# loop over the boundaries
-for color in colors:
-    # create NumPy arrays from the boundaries
-    arr = np.array(color, dtype="uint8")
+# load the image and resize it to a smaller factor so that
+# the shapes can be approximated better
+origin_image = cv2.imread("test2.png")
 
-    # find the colors within the specified boundaries and apply
-    # the mask
+low = np.array([190, 190, 190], dtype="uint8")
+up = np.array([255, 255, 255], dtype="uint8")
+mask = cv2.inRange(origin_image, low, up)
+image = cv2.bitwise_and(origin_image, origin_image, mask=255 - mask)
 
-    mask = cv2.inRange(image, errorminusvev(arr), errorplusvev(arr))
-    output = cv2.bitwise_and(image, image, mask=mask)
+resized = imutils.resize(image, width=300)
+ratio = image.shape[0] / float(resized.shape[0])
 
-    height, width, channels = output.shape
-    cv2.putText(output, color.__str__(), (width - 150, height - 20), font, 0.5, (255,255,255), 1, cv2.LINE_AA)
+# convert the resized image to grayscale, blur it slightly,
+# and threshold it
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY + 8)[1]
 
-    # show the images
-    cv2.imshow("images", np.hstack([org_image, image, output]))
-    cv2.waitKey(0)
+# find contours in the thresholded image and initialize the
+# shape detector
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+# loop over the contours
+for c in cnts:
+    # compute the center of the contour, then detect the name of the
+    # shape using only the contour
+    M = cv2.moments(c)
+    cX = int((M["m10"] / M["m00"]) * ratio)
+    cY = int((M["m01"] / M["m00"]) * ratio)
+
+    color = image[cY, cX, 0:]
+    # multiply the contour (x, y)-coordinates by the resize ratio,
+    # then draw the contours and the name of the shape on the image
+    c = c.astype("float")
+    c *= ratio
+    c = c.astype("int")
+    cv2.drawContours(origin_image, [c], -1, (0, 255, 0), 2)
+    cv2.putText(origin_image, str(color), (cY, cX), font, 0.3, (0, 0, 0), 1, cv2.LINE_AA)
+    # show the output image
+    cv2.imshow("Image", origin_image)
+
+cv2.waitKey(0)
